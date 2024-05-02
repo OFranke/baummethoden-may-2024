12c81014829e20fa7c9600360669d233e9ca7871
Oliver Franke <oliver.franke@limebit.de>
2024-05-02 11:56:58 +0200

    DIG-671 get weather forecast from api

--- src/weather_api.py
+++ src/weather_api.py
@@ -9,17 +9,41 @@ def get_city_coordinates(city):
     return response
 
 
-city = "Berlin"
+def get_weather_forecast(latitude, longitude):
+    api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min"
+    response = requests.get(api_url).json()
+    return response
+
+
+city = "Wesel"
 city_coordinates = get_city_coordinates(city)
 latitude = city_coordinates["results"][0]["latitude"]
 longitude = city_coordinates["results"][0]["longitude"]
 
-
 print(
-    "The coordinaten of",
+    "The coordinates of",
     city,
     "are: longitude:",
     longitude,
     "and latitude:",
     latitude,
 )
+
+weather_forecast = get_weather_forecast(latitude, longitude)
+
+
+time = weather_forecast["daily"]["time"]
+temperature_2m_min = weather_forecast["daily"]["temperature_2m_min"]
+temperature_2m_max = weather_forecast["daily"]["temperature_2m_max"]
+
+for i in range(len(time)):
+    print(
+        "The weather forecast for",
+        city,
+        "on",
+        time[i],
+        "is a minimum temperature of",
+        temperature_2m_min[i],
+        "and a maximum temperature of",
+        temperature_2m_max[i],
+    )
