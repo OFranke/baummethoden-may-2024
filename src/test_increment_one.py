6be2513fafd2b8bc1f0598d0db56828cd12065c7
Oliver Franke <oliver.franke@limebit.de>
2024-05-02 10:38:08 +0200

    DIG-670 fix increment_one test

--- src/test_increment_one.py
+++ src/test_increment_one.py
@@ -2,4 +2,6 @@ from increment_one import increment_one
 
 
 def test_increment_one():
-    assert increment_one(3) == 5
+    assert increment_one(4) == 5
+    assert increment_one(0) == 1
+    assert increment_one(-5) == -4
